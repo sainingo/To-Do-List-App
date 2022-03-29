edb388da1f6aa19d35dca5c5c53a6f9b8a69c614
sainingo <nickmentor421@gmail.com>
2022-03-29 17:14:45 +0300

    Dynamic populated the todo list placeholder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('working!!');
+import './styles.css';
+
+const tasks = [
+  {
+    description: 'Practice leetcode algorithms and data structures',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Afterwards set up for codewars algorithms and data structures',
+    completed: false,
+    index: 2,
+  },
+];
+
+const todoListContainer = document.querySelector('.todo-list');
+
+const todoContainer = tasks.map((task) => `
+    <ul>
+    <li><input type="checkbox"></li>
+    <li>${task.description}</li>
+    <li>&#8942;</li>
+    </ul>
+    <hr>
+    `);
+
+todoListContainer.innerHTML = todoContainer;
