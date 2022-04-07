4205f0446a265e363e1ccdbaa05f883dbf5e028c
jenadiusnicholaus <ucode888@gmail.com>
2022-04-07 13:26:14 +0300

    add remove item test

--- src/task.test.js
+++ src/task.test.js
@@ -3,12 +3,23 @@
  */
 import Task from './tasks.js';
 import displayTask from './displayTask.js';
+import updateUi from './updateui.js';
 import 'jest-localstorage-mock';
 
 const taskObj = new Task();
+const mLocalStorage = {
+  storage: {},
+  getItem: jest.fn((key) => mLocalStorage.storage[key]),
+  setItem: jest.fn((key, value) => {
+    mLocalStorage.storage[key] = value;
+  }),
+};
+Object.defineProperty(window, 'localStorage', {
+  value: mLocalStorage,
+});
 
 describe('Should Add and displayTask method & Localstorage', () => {
-  window.localStorage = Storage.prototype;
+  // window.localStorage = Storage.prototype;
   test('should add and displayContent', () => {
     taskObj.addNewTask('Test1');
     document.body.innerHTML = `
@@ -18,12 +29,8 @@ describe('Should Add and displayTask method & Localstorage', () => {
     expect(taskObj.tasks).toHaveLength(1);
   });
 
-  test('Should add to the localstorage', () => {
-    JSON.parse(localStorage.getItem('tasks'));
-    expect(localStorage).toHaveLength(1);
+  it('remove one item from localstorage', () => {
+    updateUi(Number(1));
+    expect(mLocalStorage.setItem).toBeCalledWith('tasks', '[]');
   });
-});
-
-describe('Should display', () => {
-
 });
